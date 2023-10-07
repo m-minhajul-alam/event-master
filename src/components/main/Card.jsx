d706218d745cff5f6e1f44deb195ab1064352b9c
Minhajul-alam <alam.minhajul@outlook.com>
2023-10-07 19:13:27 +0600

    Added a responsive service section with titel, description and dynamic cards. Each card have img, title, overview, feacher,price and dynamic button

--- src/components/main/Card.jsx
+++ src/components/main/Card.jsx
@@ -4,15 +4,18 @@ const Card = ({ card }) => {
     const { id, name, overview, image, price, features } = card;
 
     return (
-        <div className="flex flex-col lg:flex-row items-center max-w-4xl mx-auto rounded-lg text-blue-950 gap-5 border-2 border-blue-950 mb-3">
-            <img className="w-full lg:w-[350px] rounded-md" src={image} alt="" />
-            <div className="p-4 space-y-2">
-                <h4 className="text-2xl lg:text-3xl font-bold">{name}</h4>
-                <p className="text-xs md:text-sm"><span className="font-bold">Overview:</span> {overview}</p>
-                <p className="text-xs md:text-sm"><span className="font-bold">Features:</span> {features}</p>
-                <div className="flex justify-between items-center mt-5">
+        <div className="rounded-lg text-blue-950 border-2 border-blue-950 mb-3">
+            <img className="w-full rounded-md" src={image} alt="" />
+
+            <div className="p-4">
+                <div className="space-y-2 h-60">
+                    <h4 className="text-2xl lg:text-3xl font-bold">{name}</h4>
+                    <p className="text-xs md:text-base"><span className="font-bold">Overview:</span> {overview}</p>
+                    <p className="text-xs md:text-base"><span className="font-bold">Features:</span> {features}</p>
+                </div>
+                <div className="flex justify-between items-center mt-5 grow">
                     <p className="text-lg text-yellow-500 font-bold">{price}$ <span className="text-sm text-blue-950 font-normal">/day</span></p>
-                    <button className="btn btn-primary text-xs py-0 bg-blue-950 text-white hover:bg-black">
+                    <button className="btn btn-primary text-xs py-0 bg-blue-950 text-white hover:bg-black grow-b">
                         <Link to={`/details/${id}`}>View Details</Link></button>
                 </div>
             </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
                     <h3 className="mb-3 text-xl md:text-2xl lg:text-4xl font-bold">EventMaster's Services</h3>
                     <p className="mb-6 text-xs lg:text-base max-w-4xl mx-auto">At EventMaster, we offer a wide range of corporate event services to meet all your business needs. From Conferences and Seminars that inspire thought leadership to Trade Shows and Expos that drive industry engagement, we bring your vision to life. Our Team Building Workshops foster collaboration and creativity, while our Product Launches ignite excitement and anticipation. Explore our diverse services and let us elevate your corporate gatherings to exceptional heights.</p>
                 </div>
-                <div className="mt-10">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 mt-10">
                     {
                         cards.map(card => <Card key={card.id} card={card}></Card>)
                     }

63378643e6ac5cb0c6cc4eb6819367474f50df5c
Minhajul-alam <alam.minhajul@outlook.com>
2023-10-07 18:49:02 +0600

    Update firebase config and set up firebase deploy

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/heder/Navbar";
 
-
 const Root = () => {
     return (
         <div>
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -5,8 +5,6 @@ const Details = () => {
             <div className="bg-blue-950 h-20">
                 <h1 className="text-white text-3xl text-center font-bold pt-3">Service Details</h1>
             </div>
-
-
         </div>
     );
 };
